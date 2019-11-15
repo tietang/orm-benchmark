d804356e3f166299dadb93533abbaf1bca6a4878
tietang <wtt.2012@gmail.com>
2019-11-15 13:10:50 +0800

    add go mod

--- benchs/dbx.go
+++ benchs/dbx.go
@@ -1,157 +1,157 @@
 package benchs
 
 import (
-    "fmt"
-    "github.com/tietang/dbx"
-    "strings"
-    "time"
+	"fmt"
+	"github.com/tietang/dbx"
+	"strings"
+	"time"
 )
 
 var database *dbx.Database
 
 func init() {
-    st := NewSuite("dbx")
-    st.InitF = func() {
-        st.AddBenchmark("Insert", 2000*ORM_MULTI, DbxIOInsert)
-        st.AddBenchmark("MultiInsert 100 row", 500*ORM_MULTI, DbxIOInsertMulti)
-        st.AddBenchmark("Update", 2000*ORM_MULTI, DbxIOUpdate)
-        st.AddBenchmark("Read", 4000*ORM_MULTI, DbxIORead)
-        st.AddBenchmark("MultiRead limit 100", 2000*ORM_MULTI, DbxIOReadSlice)
+	st := NewSuite("dbx")
+	st.InitF = func() {
+		st.AddBenchmark("Insert", 2000*ORM_MULTI, DbxIOInsert)
+		st.AddBenchmark("MultiInsert 100 row", 500*ORM_MULTI, DbxIOInsertMulti)
+		st.AddBenchmark("Update", 2000*ORM_MULTI, DbxIOUpdate)
+		st.AddBenchmark("Read", 4000*ORM_MULTI, DbxIORead)
+		st.AddBenchmark("MultiRead limit 100", 2000*ORM_MULTI, DbxIOReadSlice)
 
-        settings := dbx.Settings{
-            DriverName:      "mysql",
-            User:            "root",
-            Password:        "111111",
-            Host:            "192.168.1.12:3306",
-            Database:        "orm_bench",
-            MaxOpenConns:    ORM_MAX_CONN,
-            MaxIdleConns:    ORM_MAX_IDLE,
-            ConnMaxLifetime: time.Minute * 30,
-            LoggingEnabled:  true,
-            Options: map[string]string{
-                "charset":   "utf8",
-                "parseTime": "true",
-            },
-        }
-        var err error
-        database, err = dbx.Open(settings)
-        if err != nil {
-            panic(err)
-        }
-        database.SetLogging(false)
-        //database.RegisterTable(&Model{}, "model")
-    }
+		settings := dbx.Settings{
+			DriverName:      "mysql",
+			User:            "root",
+			Password:        "111111",
+			Host:            "192.168.1.12:3306",
+			Database:        "orm_bench",
+			MaxOpenConns:    ORM_MAX_CONN,
+			MaxIdleConns:    ORM_MAX_IDLE,
+			ConnMaxLifetime: time.Minute * 30,
+			LoggingEnabled:  true,
+			Options: map[string]string{
+				"charset":   "utf8",
+				"parseTime": "true",
+			},
+		}
+		var err error
+		database, err = dbx.Open(settings)
+		if err != nil {
+			panic(err)
+		}
+		database.SetLogging(false)
+		//database.RegisterTable(&Model{}, "model")
+	}
 }
 
 func DbxIOInsert(b *B) {
-    var m *Model
-    wrapExecute(b, func() {
-        initDB()
-        m = NewModel()
-    })
+	var m *Model
+	wrapExecute(b, func() {
+		initDB()
+		m = NewModel()
+	})
 
-    for i := 0; i < b.N; i++ {
-        m.Id = 0
-        if _, err := database.Insert(m); err != nil {
-            fmt.Println(err)
-            b.FailNow()
-        }
-    }
+	for i := 0; i < b.N; i++ {
+		m.Id = 0
+		if _, err := database.Insert(m); err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+	}
 }
 
 func DbxIOInsertMulti(b *B) {
-    var ms []*Model
-    wrapExecute(b, func() {
-        initDB()
+	var ms []*Model
+	wrapExecute(b, func() {
+		initDB()
 
-        ms = make([]*Model, 0, 100)
-        for i := 0; i < 100; i++ {
-            ms = append(ms, NewModel())
-        }
-    })
+		ms = make([]*Model, 0, 100)
+		for i := 0; i < 100; i++ {
+			ms = append(ms, NewModel())
+		}
+	})
 
-    for i := 0; i < b.N; i++ {
-        nFields := 7
-        query := rawInsertBaseSQL + strings.Repeat(rawInsertValuesSQL+",", len(ms)-1) + rawInsertValuesSQL
-        args := make([]interface{}, len(ms)*nFields)
-        for j := range ms {
-            offset := j * nFields
-            args[offset+0] = ms[j].Name
-            args[offset+1] = ms[j].Title
-            args[offset+2] = ms[j].Fax
-            args[offset+3] = ms[j].Web
-            args[offset+4] = ms[j].Age
-            args[offset+5] = ms[j].Right
-            args[offset+6] = ms[j].Counter
-        }
-        _, _, err := database.Execute(query, args...)
-        if err != nil {
-            fmt.Println(err)
-            b.FailNow()
-        }
-    }
+	for i := 0; i < b.N; i++ {
+		nFields := 7
+		query := rawInsertBaseSQL + strings.Repeat(rawInsertValuesSQL+",", len(ms)-1) + rawInsertValuesSQL
+		args := make([]interface{}, len(ms)*nFields)
+		for j := range ms {
+			offset := j * nFields
+			args[offset+0] = ms[j].Name
+			args[offset+1] = ms[j].Title
+			args[offset+2] = ms[j].Fax
+			args[offset+3] = ms[j].Web
+			args[offset+4] = ms[j].Age
+			args[offset+5] = ms[j].Right
+			args[offset+6] = ms[j].Counter
+		}
+		_, _, err := database.Execute(query, args...)
+		if err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+	}
 }
 
 func DbxIOUpdate(b *B) {
-    var m *Model
+	var m *Model
 
-    wrapExecute(b, func() {
-        initDB()
-        m = NewModel()
-        rs, _ := database.Insert(m)
-        id, _ := rs.LastInsertId()
-        m.Id = int(id)
+	wrapExecute(b, func() {
+		initDB()
+		m = NewModel()
+		rs, _ := database.Insert(m)
+		id, _ := rs.LastInsertId()
+		m.Id = int(id)
 
-    })
+	})
 
-    for i := 0; i < b.N; i++ {
-        if _, err := database.Update(m); err != nil {
-            fmt.Println(err)
-            b.FailNow()
-        }
-    }
+	for i := 0; i < b.N; i++ {
+		if _, err := database.Update(m); err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+	}
 }
 
 func DbxIORead(b *B) {
-    var m *Model
-    var id int64
+	var m *Model
+	var id int64
 
-    wrapExecute(b, func() {
-        initDB()
-        m = NewModel()
-        rs, _ := database.Insert(m)
-        id, _ = rs.LastInsertId()
-    })
+	wrapExecute(b, func() {
+		initDB()
+		m = NewModel()
+		rs, _ := database.Insert(m)
+		id, _ = rs.LastInsertId()
+	})
 
-    q := &Model{Id: int(id)}
-    for i := 0; i < b.N; i++ {
-        if err := database.GetOne(q); err != nil {
-            fmt.Println(err)
-            b.FailNow()
-        }
-    }
+	q := &Model{Id: int(id)}
+	for i := 0; i < b.N; i++ {
+		if ok, err := database.GetOne(q); ok && err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+	}
 }
 
 func DbxIOReadSlice(b *B) {
-    var m *Model
-    wrapExecute(b, func() {
-        initDB()
-        m = NewModel()
+	var m *Model
+	wrapExecute(b, func() {
+		initDB()
+		m = NewModel()
 
-        for i := 0; i < 100; i++ {
-            m.Id = 0
-            if _, err := database.Insert(m); err != nil {
-                fmt.Println(err)
-                b.FailNow()
-            }
-        }
-    })
+		for i := 0; i < 100; i++ {
+			m.Id = 0
+			if _, err := database.Insert(m); err != nil {
+				fmt.Println(err)
+				b.FailNow()
+			}
+		}
+	})
 
-    for i := 0; i < b.N; i++ {
-        var models []Model
-        if err := database.Find(&models, "select * from model where id>? limit 100", 0); err != nil {
-            fmt.Println(err)
-            b.FailNow()
-        }
-    }
+	for i := 0; i < b.N; i++ {
+		var models []Model
+		if err := database.Find(&models, "select * from model where id>? limit 100", 0); err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+	}
 }
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/tietang/orm-benchmark
+
+go 1.13
+
+require (
+	github.com/astaxie/beego v1.12.0
+	github.com/coocood/mysql v0.0.0-20130514171929-d22091ecccb5 // indirect
+	github.com/coocood/qbs v0.0.0-20170418011607-8554e18a96c9
+	github.com/coopernurse/gorp v1.6.1
+	github.com/eaigner/hood v0.0.0-20131009113532-ca2c4bd5f4dd
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/go-xorm/xorm v0.7.9
+	github.com/jinzhu/gorm v1.9.11
+	github.com/jmoiron/modl v0.0.0-20160417153729-99654d091ece
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/tietang/dbx v1.0.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	upper.io/db.v3 v3.6.3+incompatible
+)
